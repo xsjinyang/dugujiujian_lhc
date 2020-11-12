8772a9572e68f2a0bcb241f5b392303c63ae6f4d
ybq <ybq@126.com>
2020-11-12 11:57:41 +0800

    ybq

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -1,7 +1,13 @@
 package com.atguigu.jianfa.dugujiujian;
 
 public class Dugu {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        System.out.println("jianfa dugujiujian1!!");
+        System.out.println("jianfa dugujiujian1!!");
+        System.out.println("jianfa dugujiujian1!!");
+        System.out.println("jianfa dugujiujian1!!");
+        System.out.println("jianfa dugujiujian1!!");
         System.out.println("jianfa dugujiujian1!!");
     }
 }
