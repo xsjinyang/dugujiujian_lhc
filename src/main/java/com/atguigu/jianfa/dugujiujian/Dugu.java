75c35dd42dc7e16ff94ade11f5652bbd76b424d3
lhc <lhc@126.com>
2020-11-12 12:03:29 +0800

    lhc

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -3,5 +3,9 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args){
         System.out.println("jianfa dugujiujian1!!");
+        System.out.println("jianfa dugujiujian2!!");
+        System.out.println("jianfa dugujiujian2!!");
+        System.out.println("jianfa dugujiujian2!!");
+        System.out.println("jianfa dugujiujian2!!");
     }
 }
